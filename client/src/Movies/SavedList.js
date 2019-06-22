cc50afbcde2cbb60868dcdbba9bf9e2f5c9e29db
Aaron T <troskaaaron@gmail.com>
2019-06-22 12:10:19 -0500

    removed unnecessary class

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,21 +1,36 @@
-import React, { Component } from 'react';
-import {Link} from 'react-router-dom'
+import React from 'react';
+import {Link, NavLink} from 'react-router-dom'
 
-export default class SavedList extends Component {
-  constructor(props) {
-    super(props);
-  }
+// export default class SavedList extends Component {
+//   constructor(props) {
+//     super(props);
+//   }
+//
+//   render(props) {
+//     console.log(this.props);
+//     return (
+//       <div className="saved-list">
+//         <h3>Saved Movies:</h3>
+//         {this.props.list.map(movie => (
+//           <NavLink to={`/movie/${movie.id}`} className="saved-movie">{movie.title}</NavLink>
+//         ))}
+//         <Link to="/" className="home-button">Home</Link>
+//       </div>
+//     );
+//   }
+// }
 
-  render(props) {
-    console.log(this.props);
-    return (
-      <div className="saved-list">
-        <h3>Saved Movies:</h3>
-        {this.props.list.map(movie => (
-          <Link to={`/movie/${movie.id}`} className="saved-movie">{movie.title}</Link>
-        ))}
-        <Link to="/" className="home-button">Home</Link>
-      </div>
-    );
-  }
+const SavedList = props => {
+  console.log(props.list);
+  return (
+    <div className="saved-list">
+      <h3>Saved Movies:</h3>
+      {props.list.map(movie => (
+        <NavLink to={`/movie/${movie.id}`} className="saved-movie" key={movie.id}>{movie.title}</NavLink>
+      ))}
+      <Link to="/" className="home-button">Home</Link>
+    </div>
+  )
 }
+
+export default SavedList;
